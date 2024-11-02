c6d290fc8eebb30bc00e306b17d2ae751d0c7244
Erik <erikalgas@gmail.com>
2024-11-02 11:44:33 -0300

    Updated files

--- Flask/routes/login.py
+++ Flask/routes/login.py
@@ -15,6 +15,9 @@ def register_login_route(app: Flask, logger: Logger):
         if data.get('token'):
             if jwt_manager.decode_jwt_token(data.get('token')):
                 return jsonify({'mensagem': 'Cliente ainda possui um token válido!'}), 409
+            else:
+                return jsonify({'mensagem': 'Token expirou! Faça login novamente!',
+                                'access_token': None}), 200
 
         usuario = User.query.filter_by(nome=data['user']).first()
 
