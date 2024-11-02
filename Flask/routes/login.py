c238a5542d26f5acef93a3d8513f90f17e10837f
Erik <erikalgas@gmail.com>
2024-11-02 16:25:20 -0300

    Updated files

--- Flask/routes/login.py
+++ Flask/routes/login.py
@@ -12,18 +12,19 @@ def register_login_route(app: Flask, logger: Logger):
         data = request.get_json()
         jwt_manager = JWTManager(app.config['SECRET_KEY'])
 
-        if data.get('token'):
-            if jwt_manager.decode_jwt_token(data.get('token')):
-                return jsonify({'mensagem': 'Cliente ainda possui um token válido!'}), 409
-            else:
-                return jsonify({'mensagem': 'Token expirou! Faça login novamente!',
-                                'access_token': None}), 200
 
         usuario = User.query.filter_by(nome=data['user']).first()
 
         if not usuario or not check_password_hash(usuario.senha_hash, data['pwd']):
             return jsonify(
                 {'auth_status': 'deauthorized', 'mensagem': 'Credenciais inválidas!'}), 406
+        
+        if data.get('token'):
+            if jwt_manager.decode_jwt_token(data.get('token')):
+                return jsonify({'mensagem': 'Cliente ainda possui um token válido!'}), 409
+            else:
+                return jsonify({'mensagem': 'Token expirou! Faça login novamente!',
+                                'access_token': None}), 200
         
         access_token = jwt_manager.generate_jwt_token(usuario.nome)
 
