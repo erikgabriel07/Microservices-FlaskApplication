db8ba7082c6f35f06ee31fb82c79f9f7c18999ad
Erik <erikalgas@gmail.com>
2024-11-04 23:06:26 -0300

    Updated docker files

--- Flask/main.py
+++ Flask/main.py
@@ -2,7 +2,7 @@ from settings.flask_app import create_app
 
 def main():
     app = create_app()
-    app.run('127.0.0.1', port=5000, debug=True) # debug somente em desenvolvimento
+    app.run('0.0.0.0', port=5000, debug=True) # debug somente em desenvolvimento
 
 
 if __name__ == '__main__':
